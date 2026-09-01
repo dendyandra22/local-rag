71807aec3ec5f92b052bef2c5c3db3c3ff8cbb00
dendyandra22 <dendyandra@gmail.com>
2026-09-01 13:56:26 +0700

    add: adding tool calling schema generator

--- rag_module/general_tool.py
+++ rag_module/general_tool.py
@@ -61,7 +61,7 @@ def sql_get_by_columns(sqldb,
     return res
 
 
-def _get_query_filter(filters: list, limit: int):
+def _get_query_filter(filters: list):
     """
     Make a SQL query for filters search.
     """
@@ -72,6 +72,7 @@ def _get_query_filter(filters: list, limit: int):
         except ValueError:
             return False
 
+    if len(filters) == 0: return ""
     query = f"WHERE "
     op_convert = {"=": "LIKE"}
 
@@ -89,7 +90,7 @@ def _get_query_filter(filters: list, limit: int):
         filter_query.append(tmp)
 
     query = query + ' AND '.join(filter_query).strip()
-    query += f" LIMIT {limit}"
+    # query += f" LIMIT {limit}"
     return query
 
 def sql_get_by_filter(sqldb, filter_list, limit, searchable_columns, return_context=True):
@@ -102,8 +103,182 @@ def sql_get_by_filter(sqldb, filter_list, limit, searchable_columns, return_cont
     if len(tmp) == 0:
         raise Exception(f"Search column invalid! Please search in this column: {searchable_columns}")
 
+    limit = limit if limit is not None else 5
 
-    query_filter = _get_query_filter(filter_list, limit)
+    query_filter = _get_query_filter(filter_list)
     res = sqldb.search_sql(query_filter, limit=limit, use_fts=False, return_context=return_context)
 
     return res
+
+def sql_get_basic_aggregation(sqldb, aggregation_list: list[dict], filter_list: list[dict], searchable_columns: list, limit: int, return_context: bool = True):
+    """
+    Do basic SQL aggregation like SUM, AVG, COUNT, MIN, MAX.
+    """
+    # # validate searchable column
+    # tmp = [fil["column"] for fil in filter_list if fil["column"] in searchable_columns]
+    # if len(tmp) == 0:
+    #     raise ValueError(f"Search column invalid! Please search in this column: {searchable_columns}")
+
+    limit = limit if limit is not None else 5
+
+    # create AGG query
+    tmp_query = []
+    for agg_data in aggregation_list:
+        agg_func = agg_data["aggregation_function"]
+        target_col = agg_data["target_col"]
+        query = f"{agg_func}({target_col})"
+        if agg_func == "COUNT_DISTINCT":
+            query = f"COUNT(DISTINCT {target_col})"
+        tmp_query.append(query)
+
+    agg_query = ", ".join(tmp_query)
+
+    # create condition/filter query
+    query_filter = _get_query_filter(filter_list)
+
+    # use column_selection to search_sql since agg is different from basic SQL query
+    res = sqldb.search_sql(query_filter,
+                           column_selection=agg_query,
+                           limit=limit,
+                           use_fts=False,
+                           return_context=return_context)
+
+    return res
+
+
+# ============================= tool calling schema =====================
+
+def _generate_column_property(columns: list):
+    properties = {}
+    for col in columns:
+        properties[col] = {
+            "type": "string",
+            "description": f"The exact value for {col} mentioned by the user. Leave null if not mentioned."
+        }
+
+    return properties
+
+
+def generate_toolcall_schema(columns: list):
+    column_property = _generate_column_property(columns)
+
+    schema = [
+        {
+            "type": "function",
+            "function": {
+                "name": "search_by_columns",
+                "description": "Finds data matching specific columns. ONLY provide arguments explicitly mentioned by the user.",
+                "parameters": {
+                    "type": "object",
+                    "properties": column_property,
+                    "required": []
+                }
+            }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "search_by_filter",
+                "description": "Filters the dataset based on conditions (like greater than, less than, or equals) and returns a specific number of rows.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "filters": {
+                            "type": "array",
+                            "description": "A list of conditions to apply to the data.",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "column": {
+                                        "type": "string",
+                                        "description": "The database column to filter.",
+                                        "enum": columns
+                                    },
+                                    "operator": {
+                                        "type": "string",
+                                        "description": "The comparison operator.",
+                                        "enum": ["=", ">", "<", ">=", "<="]
+                                    },
+                                    "value": {
+                                        "type": "string",
+                                        "description": "The value to compare against. (e.g., 'games', '7.5')"
+                                    }
+                                },
+                                "required": ["column", "operator", "value"]
+                            }
+                        },
+                        "limit": {
+                            "type": "integer",
+                            "description": "The maximum number of rows to return. Default to 10 if the user doesn't specify.",
+                            "default": 10
+                        }
+                    },
+                    "required": ["filters"]
+                }
+            }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "basic_aggregation",
+                "description": "Calculates math like MIN, MAX, AVG, SUM, or COUNT on a specific numeric column.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "aggregation_list": {
+                            "type": "array",
+                            "description": "A list of aggregation to apply to the data.",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "aggregation_function": {
+                                        "type": "string",
+                                        "description": "The type of math to do.",
+                                        "enum": ["MIN", "MAX", "AVG", "SUM", "COUNT"]
+                                    },
+                                    "target_col": {
+                                        "type": "string",
+                                        "description": "The comparison operator.",
+                                        "enum": columns
+                                    }
+                                },
+                                "required": ["aggregation_function", "target_col"]
+                            }
+                        },
+                        "filters": {
+                            "type": "array",
+                            "description": "A list of conditions to apply to the data.",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "column": {
+                                        "type": "string",
+                                        "description": "The database column to filter.",
+                                        "enum": columns
+                                    },
+                                    "operator": {
+                                        "type": "string",
+                                        "description": "The comparison operator.",
+                                        "enum": ["=", ">", "<", ">=", "<="]
+                                    },
+                                    "value": {
+                                        "type": "string",
+                                        "description": "The value to compare against. (e.g., 'games', '7.5')"
+                                    }
+                                },
+                                "required": ["column", "operator", "value"]
+                            }
+                        },
+                        "limit": {
+                            "type": "integer",
+                            "description": "The maximum number of rows to return. Default to 10 if the user doesn't specify.",
+                            "default": 10
+                        }
+                    },
+                    "required": ["aggregation_list"]
+                }
+            }
+        }
+    ]
+
+    return schema
